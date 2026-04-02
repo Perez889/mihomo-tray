8f6dc436705f9e52818b18796d5559cea65509cb
Perez889 <wuethan89@gmail.com>
2026-04-02 19:47:04 +0800

    Update mihomo_tray.go

--- mihomo_tray.go
+++ mihomo_tray.go
@@ -1,13 +1,14 @@
 package main
 
 import (
- _ "embed"
- "os"
- "os/exec"
- "path/filepath"
- "runtime"
+	_ "embed"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 
- "github.com/getlantern/systray"
+	"github.com/getlantern/systray"
 )
 
 var mihomoCmd *exec.Cmd
@@ -16,81 +17,110 @@ var mihomoCmd *exec.Cmd
 var trayIcon []byte
 
 func main() {
- systray.Run(onReady, onExit)
+	systray.Run(onReady, onExit)
 }
 
 func onReady() {
- if len(trayIcon) > 0 {
-  systray.SetTemplateIcon(trayIcon, trayIcon)
- }
+	// 设置托盘图标（关键修复）
+	if len(trayIcon) > 0 {
+		if runtime.GOOS == "windows" {
+			systray.SetIcon(trayIcon)
+		} else {
+			systray.SetTemplateIcon(trayIcon, trayIcon)
+		}
+	}
 
- // systray.SetTitle("Mihomo")
- systray.SetTooltip("Mihomo Proxy\n右键打开菜单")
+	systray.SetTooltip("Mihomo Proxy\n右键打开菜单")
 
- // 添加菜单项
- mStart := systray.AddMenuItem("启动", "启动 Mihomo")
- mOpen := systray.AddMenuItem("面板", "打开 Zashboard 面板")
- mQuit := systray.AddMenuItem("退出", "退出程序并关闭 mihomo")
+	// 菜单
+	mStart := systray.AddMenuItem("启动", "启动 Mihomo")
+	mOpen := systray.AddMenuItem("面板", "打开 Zashboard 面板")
+	mQuit := systray.AddMenuItem("退出", "退出程序并关闭 mihomo")
 
- // 菜单点击处理
- go func() {
-  for {
-   select {
-   case <-mStart.ClickedCh:
-    startMihomo()
-   case <-mOpen.ClickedCh:
-    openDashboard()
-   case <-mQuit.ClickedCh:
-    systray.Quit()
-    return
-   }
-  }
- }()
+	// 事件监听
+	go func() {
+		for {
+			select {
+			case <-mStart.ClickedCh:
+				startMihomo()
+			case <-mOpen.ClickedCh:
+				openDashboard()
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+				return
+			}
+		}
+	}()
 }
 
+// 启动 mihomo（已修复 Windows）
 func startMihomo() {
- if mihomoCmd != nil && mihomoCmd.Process != nil {
-  // 如果已经在运行，就不再重复启动
-  return
- }
+	if mihomoCmd != nil && mihomoCmd.Process != nil {
+		fmt.Println("mihomo 已在运行")
+		return
+	}
 
- baseDir := appDir()
- cmd := exec.Command(filepath.Join(baseDir, "mihomo"), "-f", filepath.Join(baseDir, "config.yaml"))
- if err := cmd.Start(); err == nil {
-  mihomoCmd = cmd
- }
+	baseDir := appDir()
+
+	exeName := "mihomo"
+	if runtime.GOOS == "windows" {
+		exeName = "mihomo.exe"
+	}
+
+	exePath := filepath.Join(baseDir, exeName)
+	configPath := filepath.Join(baseDir, "config.yaml")
+
+	fmt.Println("启动路径:", exePath)
+
+	cmd := exec.Command(exePath, "-f", configPath)
+
+	// 可选：输出日志到控制台（调试用）
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println("启动失败:", err)
+		return
+	}
+
+	mihomoCmd = cmd
+	fmt.Println("mihomo 启动成功")
 }
 
+// 打开面板
 func openDashboard() {
- url := "http://127.0.0.1:9090/ui/zashboard"
+	url := "http://127.0.0.1:9090/ui/zashboard"
 
- switch runtime.GOOS {
- case "darwin":
-  exec.Command("open", url).Start()
- case "windows":
-  exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
- default:
-  exec.Command("xdg-open", url).Start()
- }
+	switch runtime.GOOS {
+	case "darwin":
+		exec.Command("open", url).Start()
+	case "windows":
+		exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	default:
+		exec.Command("xdg-open", url).Start()
+	}
 }
 
+// 退出时关闭 mihomo
 func onExit() {
- if mihomoCmd != nil && mihomoCmd.Process != nil {
-  mihomoCmd.Process.Kill()
-  mihomoCmd.Wait()
- }
+	if mihomoCmd != nil && mihomoCmd.Process != nil {
+		fmt.Println("关闭 mihomo...")
+		mihomoCmd.Process.Kill()
+		mihomoCmd.Wait()
+	}
 }
 
+// 程序启动自动运行 mihomo
 func init() {
- // 程序启动时自动启动 mihomo
- startMihomo()
+	go startMihomo()
 }
 
+// 获取程序目录
 func appDir() string {
- exePath, err := os.Executable()
- if err != nil {
-  return "."
- }
-
- return filepath.Dir(exePath)
+	exePath, err := os.Executable()
+	if err != nil {
+		return "."
+	}
+	return filepath.Dir(exePath)
 }
