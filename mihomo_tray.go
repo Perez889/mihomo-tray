4fae3bb624155996d1e9a5aaf7c8bafa5392ccd6
Perez889 <wuethan89@gmail.com>
2026-04-02 19:36:01 +0800

    Update mihomo_tray.go

--- mihomo_tray.go
+++ mihomo_tray.go
@@ -1,48 +1,96 @@
 package main
 
 import (
-	"os/exec"
-	"syscall"
+ _ "embed"
+ "os"
+ "os/exec"
+ "path/filepath"
+ "runtime"
 
-	"github.com/getlantern/systray"
+ "github.com/getlantern/systray"
 )
 
+var mihomoCmd *exec.Cmd
+
+//go:embed clash.ico
+var trayIcon []byte
+
 func main() {
-	systray.Run(onReady, onExit)
+ systray.Run(onReady, onExit)
 }
 
 func onReady() {
-	// 使用系统默认图标（避免 clash.ico 加载失败导致托盘不出现）
-	systray.SetTitle("Mihomo")
-	systray.SetTooltip("Mihomo Proxy\n双击或右键打开菜单")
+ if len(trayIcon) > 0 {
+  systray.SetTemplateIcon(trayIcon, trayIcon)
+ }
 
-	mOpen := systray.AddMenuItem("面板", "打开 Zashboard 面板")
-	mQuit := systray.AddMenuItem("退出", "退出程序并关闭 mihomo")
+ // systray.SetTitle("Mihomo")
+ systray.SetTooltip("Mihomo Proxy\n右键打开菜单")
 
-	go func() {
-		for {
-			select {
-			case <-mOpen.ClickedCh:
-				openDashboard()
-			case <-mQuit.ClickedCh:
-				systray.Quit()
-				return
-			}
-		}
-	}()
+ // 添加菜单项
+ mStart := systray.AddMenuItem("启动", "启动 Mihomo")
+ mOpen := systray.AddMenuItem("面板", "打开 Zashboard 面板")
+ mQuit := systray.AddMenuItem("退出", "退出程序并关闭 mihomo")
+
+ // 菜单点击处理
+ go func() {
+  for {
+   select {
+   case <-mStart.ClickedCh:
+    startMihomo()
+   case <-mOpen.ClickedCh:
+    openDashboard()
+   case <-mQuit.ClickedCh:
+    systray.Quit()
+    return
+   }
+  }
+ }()
+}
+
+func startMihomo() {
+ if mihomoCmd != nil && mihomoCmd.Process != nil {
+  // 如果已经在运行，就不再重复启动
+  return
+ }
+
+ baseDir := appDir()
+ cmd := exec.Command(filepath.Join(baseDir, "mihomo"), "-f", filepath.Join(baseDir, "config.yaml"))
+ if err := cmd.Start(); err == nil {
+  mihomoCmd = cmd
+ }
 }
 
 func openDashboard() {
-	exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://127.0.0.1:9090/ui/zashboard").Start()
+ url := "http://127.0.0.1:9090/ui/zashboard"
+
+ switch runtime.GOOS {
+ case "darwin":
+  exec.Command("open", url).Start()
+ case "windows":
+  exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+ default:
+  exec.Command("xdg-open", url).Start()
+ }
 }
 
 func onExit() {
-	exec.Command("taskkill", "/f", "/im", "mihomo.exe").Run()
+ if mihomoCmd != nil && mihomoCmd.Process != nil {
+  mihomoCmd.Process.Kill()
+  mihomoCmd.Wait()
+ }
 }
 
 func init() {
-	// 启动 mihomo（隐藏窗口）
-	cmd := exec.Command("mihomo.exe", "-f", "config.yaml")
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	cmd.Start()
+ // 程序启动时自动启动 mihomo
+ startMihomo()
+}
+
+func appDir() string {
+ exePath, err := os.Executable()
+ if err != nil {
+  return "."
+ }
+
+ return filepath.Dir(exePath)
 }
